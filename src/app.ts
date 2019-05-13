f25bc7bdf411468c930830ca86d717885aa32f2f
c3st4y <36391333+c3st4y@users.noreply.github.com>
2019-05-13 07:16:54 -0400

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ const userBill = document.getElementById('userBill') as HTMLInputElement;
 const billAmount = document.getElementById('billAmount') as HTMLInputElement;
 const tipPercentage = document.getElementById('tipPercentage') as HTMLInputElement;
 const headerTip = document.getElementById('headerTip') as HTMLInputElement;
+const billAmt = userBill.valueAsNumber;
 
 const amountOfTip = document.getElementById('amountOfTip');
 const totalPaid = document.getElementById('totalPaid');
@@ -16,16 +17,25 @@ const tenPercentButton = document.getElementById('tenPercent') as HTMLInputEleme
 const fifteenPercentButton = document.getElementById('fifteenPercent') as HTMLInputElement;
 const twentyPercentButton = document.getElementById('twentyPercent') as HTMLInputElement;
 
-function doTenPercent() {
+const tenPercentSelected = document.getElementById('tenPercent') as HTMLDivElement;
+const fifteenPercentSelected = document.getElementById('fifteenPercent') as HTMLDivElement;
+const twentyPercentSelected = document.getElementById('twentyPercent') as HTMLDivElement;
 
+
+function doTenPercent() {
+  
     const billAmt = userBill.valueAsNumber;
     const tip = (multiply(billAmt, .10));
-    
+
     headerTip.innerText = "10%";
     billAmount.innerText = '$' + billAmt.toFixed(2);
     tipPercentage.innerText = "10%";
     amountOfTip.innerText = '$' +tip.toFixed(2);
     totalPaid.innerText = '$' + (add( billAmt, tip)).toFixed(2);
+    tenPercentSelected.classList.add('selected');
+    fifteenPercentSelected.classList.remove('selected');
+    twentyPercentSelected.classList.remove('selected');
+
 }
 
 function doFifteenPercent() {
@@ -38,6 +48,9 @@ function doFifteenPercent() {
     tipPercentage.innerText = "15%";
     amountOfTip.innerText = '$' +tip.toFixed(2);
     totalPaid.innerText = '$' + (add( billAmt, tip)).toFixed(2);
+    tenPercentSelected.classList.remove('selected');
+    fifteenPercentSelected.classList.add('selected');
+    twentyPercentSelected.classList.remove('selected');
 }
 
 function doTwentyPercent() {
@@ -50,10 +63,31 @@ function doTwentyPercent() {
     tipPercentage.innerText = "20%";
     amountOfTip.innerText = '$' +tip.toFixed(2);
     totalPaid.innerText = '$' + (add( billAmt, tip)).toFixed(2);
+    tenPercentSelected.classList.remove('selected');
+    fifteenPercentSelected.classList.remove('selected');
+    twentyPercentSelected.classList.add('selected');
 }
+
+function clearCalculator() {
+
+    if (userBill.value.length === 0) {
+  
+    headerTip.innerText = '';
+    billAmount.innerText = '';
+    tipPercentage.innerText = '';
+    amountOfTip.innerText = '';
+    totalPaid.innerText = '';
+
+    tenPercentSelected.classList.remove('selected');
+    fifteenPercentSelected.classList.remove('selected');
+    twentyPercentSelected.classList.remove('selected');
+    }
+}
+
 
 tenPercentButton.addEventListener('click', doTenPercent);
 fifteenPercentButton.addEventListener('click', doFifteenPercent);
 twentyPercentButton.addEventListener('click', doTwentyPercent);
+userBill.addEventListener('mousemove', clearCalculator);
 
 }
